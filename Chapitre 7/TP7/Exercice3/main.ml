285f09aaad7915c8f2d600944ebb4588bb9ecf3c
jspkoiternions <151717900+jspkoiternions@users.noreply.github.com>
2024-01-24 09:13:47 +0100

    suite ex 3 tp7

--- Chapitre 7/TP7/Exercice3/main.ml
+++ Chapitre 7/TP7/Exercice3/main.ml
@@ -33,3 +33,16 @@ let somme t1 t2 =
   done;
   t.(n) <- !ret;
   t;;
+
+
+
+let somme_liste l1 l2 =
+  let rec somme_aux l1 l2 ret =
+    match (l1,l2) with 
+    | ([],[]) -> [ret]
+    | ([],_) -> l1
+    | (_,[]) -> l2
+    | (t1::s1,t2::s2) -> let ret2 = ret_trois_booleens t1 t2 ret in
+        (somme_trois_booleens t1 t2 ret)::(somme_aux s1 s2 ret2)
+  in somme_aux l1 l2 false ;;
+
