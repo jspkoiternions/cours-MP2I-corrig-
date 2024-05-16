be2230998ba41057f561fed6808579ee74343c67
jspkoiternions <151717900+jspkoiternions@users.noreply.github.com>
2024-05-16 14:26:33 +0000

    fonctions c td9/cours

--- Chapitre14-prog_dynamique/TD9/td9.c
+++ Chapitre14-prog_dynamique/TD9/td9.c
@@ -4,19 +4,6 @@
 struct result{int *t;int size;};
 typedef struct result result;
 
-result rendu_monnaie_glouton(int n, int p[n], int x);
-
-int main(){
-
-  int v = 6;
-  int p[] = {1,3,4};
-  result res = rendu_monnaie_glouton(3,p,v);
-  for (int i = 0; i < res.size-1; i++){
-    printf("%d",res.t[i]);
-  }
-  return 0;
-}
-
 result rendu_monnaie_glouton(int n, int p[n], int x){
   //en supposant le tableau p 
   int *t = malloc(sizeof(struct result));
@@ -34,3 +21,31 @@ result rendu_monnaie_glouton(int n, int p[n], int x){
   result res = {t, n};
   return res;
 }
+
+int **creer_matrice(int n, int m){
+  int **p = malloc(sizeof(int*)*n);
+  for (int i = 0; i<n i ++){
+    p[i] = malloc(sizeof(int)*m);
+  }
+  return p ;
+}
+
+void detruire_matrice(int **p,int n, int m){
+  for(int i = 0; i<n;i++){
+    free(p[i]);
+  }
+  free(p);
+}
+
+
+int main(){
+
+  int v = 6;
+  int p[] = {1,3,4};
+  result res = rendu_monnaie_glouton(3,p,v);
+  for (int i = 0; i < res.size-1; i++){
+    printf("%d",res.t[i]);
+  }
+  return 0;
+}
+
