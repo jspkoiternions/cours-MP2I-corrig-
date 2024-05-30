270fd8908fb54e32a8bc974f12bc0c84ebf7d0a1
jspkoiternions <151717900+jspkoiternions@users.noreply.github.com>
2024-05-30 15:01:35 +0000

    suite TP15

--- Chapitre15_graphe/TP15/TP15.c
+++ Chapitre15_graphe/TP15/TP15.c
@@ -1,21 +1,69 @@
-#include <stdbool.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<assert.h>
 
+struct mat_graphe{
+    int taille;
+    bool **mat;
+};
+typedef struct mat_graphe mat_graphe;
 
-typedef struct lst_ch
-{ int value ; struct lst_ch *next;}
-list;
+struct l_sommet{
+    int valeur;
+    struct l_sommet *next;
+};
+typedef struct l_sommet l_sommet;
 
-typedef struct type mat_graph
-{
-  int taille;
-  bool **mat;
-} mat_graph
+l_sommet *ajout_l_sommet(l_sommet *l,int e){
+    l_sommet *l1 = malloc(sizeof(l_sommet));
+    l1->valeur = e;
+    l1->next = l;
+    return l1;
+}
 
-typedef struct liste_graphe
-{
-  int taille;
-  l_sommet *list;
-} liste_graphe;
+struct liste_graphe{
+    int taille;
+    l_sommet *tab;
+};
+typedef struct liste_graphe liste_graphe;
+
+liste_graphe *creer_liste_graphe(int n){
+    liste_graphe *l = malloc(sizeof(liste_graphe));
+    l->taille = n;
+    l_sommet *tab1 = malloc(sizeof(l_sommet)*n);
+    l->tab = tab1;
+    return l;
+}
+
+struct pile{
+    int valeur;
+    struct pile* next;
+};
+typedef struct pile pile;
+
+bool isempty_pile(pile *p){
+    return (p == NULL);
+}
+
+pile *ajout_pile(pile *p,int e){
+    pile *p1 = malloc(sizeof(pile));
+    p1->valeur = e;
+    p1->next = p;
+    return p1;
+}
+
+pile *depiler(pile *p){
+    assert(!isempty_pile(p));
+    pile *p1 = p->next;
+    free(p);
+    p = p1;
+    return p;
+}
+
+pile *creer_pile(){
+    pile *p = NULL;
+    return p;
+}
 
 bool **creer_matrice(int n,int m){
     bool *p = malloc(sizeof(bool*)*n);
@@ -47,4 +95,43 @@ void supprimer_mat_graphe(*mat_graph m1){
     detruire_matrice(m1->mat,m1->taille);
     free(m);
 }
+
+
+void ajout_arete_go (mat_graphe *m, int s1, int s2){
+    int n = m->taille ;
+    assert(s1>=0 && s1<n && s2>= 0 && s2<n);
+    boot **m1 = m->mat;
+    m1[s1][s2] = true;
+}
+
+void ajout_arete_gno (mat_graphe *m, int s1, int s2){
+    int n = m->taille ;
+    assert(s1>=0 && s1<n && s2>= 0 && s2<n);
+    boot **m1 = m->mat;
+    m1[s1][s2] = true;
+    m1[s2][s1] = true;
+}
+
+int nb_arete_go (mat_graphe *m){
+    int n = m->taille;
+    bool**m1 = m->mat;
+    int compteur = 0;
+    for (int i = 0 ; i<n; i++){
+        for(int j = 0;j<n;j++){
+            if (m1[i][j]){
+                compteur++;
+            }
+        }
+    }
+    return compteur;
+}
+
+int nb_arete
+
+int main(){
+
+    return 0;
+}
+
+
 
